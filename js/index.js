c1aec0c9fac1af2d82d925793a43041aa8e0b73e
Derek Truesdale <dtruesdale37@gmail.com>
2023-06-02 15:14:56 -0500

    finished search users

--- js/index.js
+++ js/index.js
@@ -1,11 +1,46 @@
-Accept: application/vnd.github.v3+json
+const form = document.getElementById('github-form')
 
-function fetchApi(){
-fetch('https://docs.github.com/en/rest?apiVersion=2022-11-28')
-.then((res)=>res.json())
-.then((data)=>data.forEach(user=>searchApi(user)))
-}
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-function searchApi(user){
-    
-}
+    const search = document.getElementById('search').value
+
+    const userName= search.split(' ').join('') 
+
+    fetch(`https://api.github.com/search/users?q=${userName}`,{
+        method: 'GET',
+        headers:{
+            Accept: 'application/vnd.github.v3+json'
+        }
+    })
+    .then((res)=> res.json())
+    .then((data) => {
+        console.log(data)
+        for(let i=0;i<data.items.length;i++){
+            console.log(data.items[i].login)
+            const li =document.createElement('li')
+            const userList= document.getElementById('user-list')
+            li.innerHTML=`${data.items[i].login}`
+            userList.appendChild(li)
+        }
+    })
+
+    fetch(`https://api.github.com/search/users?q=${userName}/repos`,{
+        method: 'GET',
+        headers:{
+            Accept: 'application/vnd.github.v3+json'
+        }
+    })
+    .then((res)=> res.json())
+    .then((data) => {
+        console.log(data)
+        for(let i=0;i<data.items.length;i++){
+            console.log(data.items[i].length)
+            const li =document.createElement('li')
+            const repoList= document.getElementById('repos-list')
+            li.innerHTML=`${data.items[i].length}`
+            repoList.appendChild(li)
+        }
+    })
+
+})
